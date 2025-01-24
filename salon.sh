064cdc38e6ddc7661de76c9e0c352e6cf2f7136d
Mathias Yanqui <167260271+gooseyO3@users.noreply.github.com>
2025-01-24 12:56:43 -0500

    salon.sh

--- salon.sh
+++ salon.sh
@@ -9,63 +9,46 @@ MAIN_MENU() {
   then
     echo -e "\n$1"
   fi
-# show available services
   SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
-  # if there is no service available
   if [[ -z $SERVICES ]]
   then
-    echo "Sorry, we don't have any service right now"
-  # case there it is, show them formated
+    echo "Sorry, we don't have available services"
   else
     echo -e "$SERVICES" | while read SERVICE_ID BAR NAME
     do
       echo "$SERVICE_ID) $NAME"
     done
-  # get customer choice
-  read SERVICE_ID_SELECTED
-  # if the choice is not a number
-    if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+  read SELECTED_SERVICE
+    if [[ ! $SELECTED_SERVICE =~ ^[0-9]+$ ]]
     then
-    # send to main menu
-      MAIN_MENU "Sorry, that is not a valid number! Please, choose again."
+      MAIN_MENU "Sorry, not a valid number! Please, enter a valid option."
     else
-      VALID_SERVICE=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_SELECTED")
-      # if it is a number but not the valid ones
-      if [[ -z $VALID_SERVICE ]]
+      CHOOSED_SERVICE=$($PSQL "SELECT service_id FROM services WHERE service_id = $SELECTED_SERVICE")
+      if [[ -z $CHOOSED_SERVICE ]]
       then
-      # send to main menu
-        MAIN_MENU "I could not find that service. What would you like today?"
+        MAIN_MENU "Non-existent service. What would you like today?"
       else
-      # get customer phone number
         echo -e "\nWhat's your phone number?"
         read CUSTOMER_PHONE
-        # check if is a new customer or not
         CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
-        # if is a new customer
           if [[ -z $CUSTOMER_NAME ]]
           then
-          # get the name, phone and incluid it to the table with the selected service
-          echo -e "\nI don't have a record for that phone number, what's your name?"
+          echo -e "\nWhat's your name?"
           read CUSTOMER_NAME
-          CUSTOMER_INFO_INCLUSION=$($PSQL "INSERT INTO customers(phone, name) VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
-          SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
-          # get the time the customer wants to appoint
-          echo "What time would you like your $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
+          CUSTOMER_INFO=$($PSQL "INSERT INTO customers(phone, name) VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+          SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SELECTED_SERVICE")
+          echo "What time would you like your $(echo $CHOOSED_SERVICE | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
           read SERVICE_TIME
-          # update the appointment table 
           CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
-          APPOINTMENT_INCLUSION=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
-          echo -e "\nI have put you down for a $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
-        # case is an old customer
+          APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SELECTED_SERVICE, '$SERVICE_TIME')")
+          echo -e "\nI have put you down for a $(echo $CHOOSED_SERVICE | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
         else
-        # get the service name and ask for the time the customer wants to appoint
-        SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
-        echo "What time would you like your $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
+        SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SELECTED_SERVICE")
+        echo "What time would you like your $(echo $CHOOSED_SERVICE | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
         read SERVICE_TIME
-        # update the appointment table 
         CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
-        APPOINTMENT_INCLUSION=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
-        echo -e "\nI have put you down for a $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
+        APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SELECTED_SERVICE, '$SERVICE_TIME')")
+        echo -e "\nI have put you down for a $(echo $CHOOSED_SERVICE | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
         fi
       fi
     fi
